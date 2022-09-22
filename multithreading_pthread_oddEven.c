bf5c1a8f2e7c68cd8d78d141f79d6600e6b009b4
ARP1208 <79012440+ARP1208@users.noreply.github.com>
2022-09-22 10:54:29 +0530

    Add files via upload

---
+++ multithreading_pthread_oddEven.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+void *thread1(){
+    printf("\nThis is Thread 1\n");
+    for (int i = 0; i <= 100; i++)
+    {
+        if (i % 2 != 0)
+        {
+            printf("%d\t");
+        }
+    }
+}
+
+void *thread2(){
+    printf("\nThis is Thread 1\n");
+    for (int i = 0; i <= 100; i++)
+    {
+        if (i % 2 == 0)
+        {
+            printf("%d\t");
+        }
+    }
+}
+
+int main()
+{
+    int status;
+    pthread_t tid1, tid2;
+
+    pthread_create(&tid1, NULL, thread1, NULL);
+    pthread_create(&tid2, NULL, thread2, NULL);
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+
+    return 0;
+    
+}
