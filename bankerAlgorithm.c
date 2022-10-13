4e94d7271e1d896c3efcad774660bad3cd5b8182
ARP1208 <79012440+ARP1208@users.noreply.github.com>
2022-10-13 09:45:45 +0530

    Update bankerAlgorithm.c

--- bankerAlgorithm.c
+++ bankerAlgorithm.c
@@ -1,78 +1,85 @@
 // Banker's Algorithm
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
-    // P0, P1, P2, P3, P4 are the Process names here
- 
-    int n, m, i, j, k;
-    n = 5; // Number of processes
-    m = 3; // Number of resources
-    int alloc[5][3] = { { 0, 1, 0 }, // P0    // Allocation Matrix
-                        { 2, 0, 0 }, // P1
-                        { 3, 0, 2 }, // P2
-                        { 2, 1, 1 }, // P3
-                        { 0, 0, 2 } }; // P4
- 
-    int max[5][3] = { { 7, 5, 3 }, // P0    // MAX Matrix
-                      { 3, 2, 2 }, // P1
-                      { 9, 0, 2 }, // P2
-                      { 2, 2, 2 }, // P3
-                      { 4, 3, 3 } }; // P4
- 
-    int avail[3] = { 3, 3, 2 }; // Available Resources
- 
-    int f[n], ans[n], ind = 0;
-    for (k = 0; k < n; k++) {
-        f[k] = 0;
+  int p, c, count = 0, i, j, alc[5][3], max[5][3], need[5][3], safe[5], available[3], done[5], terminate = 0;
+  printf("Enter the Number of Processes and Resources: ");
+  scanf("%d %d", & p, & c);
+  printf("Enter Allocation of Resource of all Process %dx%d Matrix: ", p, c);
+  for (i = 0; i < p; i++) {
+    for (j = 0; j < c; j++) {
+      scanf("%d", & alc[i][j]);
     }
-    int need[n][m];
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++)
-            need[i][j] = max[i][j] - alloc[i][j];
+  }
+  printf("Enter the Max Resource Process Required %dx%d Matrix: ", p, c);
+  for (i = 0; i < p; i++) {
+    for (j = 0; j < c; j++) {
+      scanf("%d", & max[i][j]);
     }
-    int y = 0;
-    for (k = 0; k < 5; k++) {
-        for (i = 0; i < n; i++) {
-            if (f[i] == 0) {
- 
-                int flag = 0;
-                for (j = 0; j < m; j++) {
-                    if (need[i][j] > avail[j]){
-                        flag = 1;
-                         break;
-                    }
-                }
- 
-                if (flag == 0) {
-                    ans[ind++] = i;
-                    for (y = 0; y < m; y++)
-                        avail[y] += alloc[i][y];
-                    f[i] = 1;
-                }
-            }
-        }
+  }
+  printf("Enter the  Available Resource: ");
+  for (i = 0; i < c; i++)
+    scanf("%d", & available[i]);
+
+  printf("\nNeed Resources Matrix are: \n");
+  for (i = 0; i < p; i++) {
+    for (j = 0; j < c; j++) {
+      need[i][j] = max[i][j] - alc[i][j];
+      printf("%d\t", need[i][j]);
     }
-    
-    int flag = 1;
-    
-    for(int i=0;i<n;i++)
-    {
-        if(f[i]==0)
-        {
-            flag=0;
-            printf("The following system is not safe");
+    printf("\n");
+  }
+
+  for (i = 0; i < p; i++) {
+    done[i] = 0;
+  }
+
+  while (count < p) {
+    for (i = 0; i < p; i++) {
+      if (done[i] == 0) {
+        for (j = 0; j < c; j++) {
+          if (need[i][j] > available[j])
             break;
         }
+        
+        if (j == c) {
+          safe[count] = i;
+          done[i] = 1;
+          
+          for (j = 0; j < c; j++) {
+            available[j] += alc[i][j];
+          }
+          count++;
+          terminate = 0;
+        } else {
+          terminate++;
+        }
+      }
     }
-    
-    if(flag==1)
-    {
-        printf("Following is the SAFE Sequence\n");
-        for (i = 0; i < n - 1; i++)
-        printf(" P%d ->", ans[i]);
-        printf(" P%d", ans[n - 1]);
+    if (terminate == (p - 1)) {
+      printf("Safe Sequence does not exist: ");
+      break;
     }
-    
-    return (0);
+
+  }
+  if (terminate != (p - 1)) {
+    printf("\nAvailable Resource after Completion: \n");
+    for (i = 0; i < c; i++) {
+      printf("%d\t", available[i]);
+    }
+    printf("\n Safe Sequence are: \n");
+    for (i = 0; i < p; i++) {
+      printf("p%d\t", safe[i]);
+    }
+  }
+
+  return 0;
 }
+
+/*
+Enter the Number of Processes and Resources: 5 3
+Enter Allocation of Resource of all Process 5x3 Matrix:  0 1 0 2 0 0 3 0 2 2 1 1 0 0 2
+Enter the Max Resource Process Required 5x3 Matrix: 7 5 3 3 2 2 9 0 2 2 2 2 4 3 3 
+Enter the  Available Resource: 3 3 2
+*/
